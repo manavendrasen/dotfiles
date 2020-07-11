59c0511336849582dad7fe2ae61c1b9c1b5a68e5
manavendrasen <manavendra.199302058@muj.manipal.edu>
2020-07-11 17:46:15 +0530

    + dev mode alias

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -1,5 +1,7 @@
 alias update="sudo apt update"
-alias upgrade="sudo apt upgrade"
+alias upgrade="pkcon update"
 alias inst="sudo apt install "
 alias ll="la -la"
-
+alias cloq="tty-clock -cDsC 6"
+alias gdep="git push heroku master && git push origin master"
+alias dev="figlet dev-mode activated | lolcat ; code && /usr/bin/brave-browser-stable --force-device-scale-factor=1.2 %U"
