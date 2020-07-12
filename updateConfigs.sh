36a6fc9bc1abf7762c61b5c385a9cf01b6da3513
manavendrasen <manavendra.199302058@muj.manipal.edu>
2020-07-13 00:02:09 +0530

    Auto-update script (Copies the config from system to git folder)

---
+++ updateConfigs.sh
@@ -0,0 +1,4 @@
+cp ~/.zshrc ~/mydotfiles/zsh/.zshrc 
+cp ~/.aliases.zsh ~/mydotfiles/zsh/.aliases.zsh
+cp ~/.config/i3status/config ~/mydotfiles/i3wm/i3status/config 
+cp ~/.config/i3/config ~/mydotfiles/i3wm/i3/config
