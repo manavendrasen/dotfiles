7a8c1fb9106bc3fd86fdcce9abf35687636e77c9
manavendrasen <manavendra.199302058@muj.manipal.edu>
2020-12-28 23:15:04 +0530

    Updated : removed i3 from config

--- updateConfigs.sh
+++ updateConfigs.sh
@@ -1,4 +1,6 @@
-cp ~/.zshrc ~/mydotfiles/zsh/.zshrc 
-cp ~/.aliases.zsh ~/mydotfiles/zsh/.aliases.zsh
-cp ~/.config/i3status/config ~/mydotfiles/i3wm/i3status/config 
-cp ~/.config/i3/config ~/mydotfiles/i3wm/i3/config
+cp ~/.zshrc ~/dotfiles/zsh/.zshrc 
+cp ~/.aliases.zsh ~/dotfiles/zsh/.aliases.zsh
+# cp ~/.config/i3status/config ~/mydotfiles/i3wm/i3status/config 
+# cp ~/.config/i3/config ~/mydotfiles/i3wm/i3/config
+
+echo "updated configs"
