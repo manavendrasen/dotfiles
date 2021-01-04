2f54bd1041c4becc57f0a4c7a28c6616f2c0a2f4
manavendrasen <manavendra.199302058@muj.manipal.edu>
2021-01-04 17:25:09 +0530

    Added : nvim update command

--- updateConfigs.sh
+++ updateConfigs.sh
@@ -3,4 +3,6 @@ cp ~/.aliases.zsh ~/dotfiles/zsh/.aliases.zsh
 # cp ~/.config/i3status/config ~/mydotfiles/i3wm/i3status/config 
 # cp ~/.config/i3/config ~/mydotfiles/i3wm/i3/config
 
+# Update nvim init.vim
+cp -r ~/.config/nvim/* ./nvim
 echo "updated configs"
